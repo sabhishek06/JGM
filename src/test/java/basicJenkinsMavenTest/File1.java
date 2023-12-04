c3961fc8a9ff1629673d6af02a28c711a3e6d0f4
Abhishek <s.abhishek2505@gmail.com>
2023-12-04 19:44:55 +0530

    First commit

---
+++ src/test/java/basicJenkinsMavenTest/File1.java
@@ -0,0 +1,13 @@
+package basicJenkinsMavenTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class File1 {
+	
+	@Test
+	public static void test1() {
+		Reporter.log("Hi How are you ? ---->>>>>>>>>>",true );
+	}
+
+}
